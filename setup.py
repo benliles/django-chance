7718610d02cff64ee4d21e815a93140d7d144c33
Benjamin Liles <benliles@arch.tamu.edu>
2013-06-24 14:44:06 -0500

    Version metadata for 0.4

--- setup.py
+++ setup.py
@@ -4,27 +4,25 @@ from setuptools import setup, find_packages
 
 
 
-version = '0.3.5'
+version = '0.4'
 
 setup(
     name = 'django-chance',
     version = version,
-    description = "Django CMS Plugin for Google Forms",
+    description = "Django CMS conference application",
     long_description = open(join(dirname(__file__), 'README.rst')).read() + "\n" + 
                        open(join(dirname(__file__), 'HISTORY.rst')).read(),
     classifiers = [
         "Framework :: Django",
-        "Development Status :: 3 - Alpha",
-        #"Development Status :: 4 - Beta",
-        #"Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: Apache Software License"],
     keywords = 'django cms plugin events conference',
     author = 'Benjamin Liles',
-    author_email = 'benliles@gmail.com',
-    url = 'https://github.com/benliles/django-chance',
+    author_email = 'benliles@pytexas.net',
+    url = 'https://github.com/pytexas/django-chance',
     packages = find_packages(),
     include_package_data = True,
     zip_safe = False,
